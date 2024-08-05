1dc7d40561d1558c1308c4c057ef6142560be6b5
pinky-nadar <umamaheshwari4795@gmail.com>
2024-08-05 19:47:37 +0530

    themes

--- movie/src/app/components/Header.jsx
+++ movie/src/app/components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import MenuItem from './MenuItem';
 import { AiFillHome } from "react-icons/ai";
+import ThemeSwitch from './ThemeSwitch';
 
 export default function Header() {
   return (
@@ -9,10 +10,14 @@ export default function Header() {
             <MenuItem title="home" address="/" Icon={ <AiFillHome/> }/>
             <MenuItem title="about" address="/about" Icon={ <AiFillHome/> }/>
         </div>
-        <Link href={'/'} className='flex gap-1 items-center'>
+        <div className='flex gap-1 items-center'>
+          <ThemeSwitch/>
+          <Link href={'/'} className='flex gap-1 items-center'>
             <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
             <span className='text-xl hidden sm:inline'>Clone</span>
-        </Link>
+          </Link>
+        </div>
+        
     </div>
   );
 }
--- movie/src/app/components/MenuItem.jsx
+++ movie/src/app/components/MenuItem.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function MenuItem({ title, address, Icon }) {
   return (
     <Link href={address} className='hover:text-amber-600'>
-        <Icon className="" />
+        {/* <Icon className="" /> */}
         <p className='uppercase hidden text-sm sm:inline'>{title}</p>
     </Link>
   );
